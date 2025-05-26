e5f15a726ebd01a3e9d45c65d845080996130c65
Carla135 <carlaandrealopezs@gmail.com>
2025-05-26 12:05:55 -0400

    Add files via upload

---
+++ practica5/server/server.js
@@ -0,0 +1,17 @@
+import express from 'express';
+import cors from 'cors';
+import rutas from './routes/routes.js'
+
+
+const app=express();
+
+app.use(cors());
+app.use(express.json());
+
+app.use('/api/medicamento',rutas);
+
+const PORT=3000;
+app.listen(PORT,()=>{
+    console.log('Corriendo en http://localhost:${PORT}')
+    
+})
